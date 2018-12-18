1e982e807cf928605a4dc8f6b6008fe02e521479
wangjiaJessie <32219092+wangjiaJessie@users.noreply.github.com>
2018-12-18 09:38:54 +0800

    Add files via upload

--- task2/Contents.swift
+++ task2/Contents.swift
@@ -1,204 +1,64 @@
 import UIKit
-var  primes = [Int]() //定义一个数组用来存储1到10000的质数
-for i in 2...10000{
-    var isPrime = true
-    for j in 2..<i{
-        if i%j==0{
-            isPrime = false
-        }
-    }
-    if isPrime {
-        primes.append(i)
-    }
+//定义一个字典
+var dictionary = ["zhangsan":18,"lisi":17,"wangwu":20]
+//将字典里面的values提取出来
+var restore = Array( dictionary.values)
+//用map函数将整数数组转换为字符串数组
+let arry2 = restore.map{
+    "\($0)"
 }
-print("1到10000的质数如下：")
-print(primes)
-//first method
-//func compare(x:Int,y:Int) ->Bool{
-//    return x > y; //return x<y 从小到大的顺序
-//}
-print("按照从大到小的顺序排序为")
-//primes.sort(by:compare)
-//print(primes)
-//second method
-//primes.sort(by:{(x:Int,y:Int)->Bool in return x>y})
-//print(primes)
-//third method
-//primes.sort(by:{(x,y)in x>y})
-//print(primes)
-//forth method
-//primes.sort(by:{$0>$1}) //从大到小的顺序
-//primes.sort(by:{$0<$1}) 从小到大的顺序
-//print(primes)
-//five method
-//primes.sort(by:>)
-//primes.sort(by:<)
-//six method
-primes.sort{ $0 > $1 }
-//primes.sort{ $0< $1 }
-print(primes)
-enum Gender{
-    case male
-    case female
+print(arry2)
+var string = ["12","ty45","89","yhd","67rt","20"]
+let chooseData = string.filter{ Int($0) != nil}
+print(chooseData)
+let connectString = string.reduce(""){connectString,letter in "\(connectString),\(letter)"}
+print("合并成一句话为：\(connectString)")
+var integer = [1,2,3,456,0]
+let result = integer.reduce((max:Int.min,min:Int.max,sum:0)){
+    (result,i) in return (max(result.max,i),min(result.min,i),result.sum+i)
+}
+print("最大值：\(result.max)")
+print("最小值：\(result.min)")
+print("总数之和：\(result.sum)")
+var funcarry = [Any]()
+func add(number1: Int) ->Int{
+    return number1+1
+}
+func stringconn(string:String) ->String{
+    return string+"1"
+}
+funcarry.append(add)
+funcarry.append(stringconn)
+for i in 0...1{
+    if funcarry[i] is ((Int)->(Int)){
+        print("第\(i+1)个func is type:(Int)->(Int)")
+    }
     
 }
-//定义协议
-enum Department {
-    case Math
-    case Computer
-    case Chinese
-}
-protocol SchoolProtocol {
-    var department : Department {get}
-    func lendBook()
-}
-class Person: CustomStringConvertible {
-    var firstName: String
-    var lastName: String
-    var age: Int
-    var gender: Gender
-    var fullName: String{ get{return firstName + lastName }}
-    //初始化
-    init(firstName: String, lastName: String, age: Int, gender: Gender){
-        self.firstName = firstName
-        self.lastName = lastName
-        self.age = age
-        self.gender = gender
-    }
-    //便利初始化
-    convenience init(firstName: String, age: Int, gender: Gender) {
-        self.init(firstName: firstName, lastName: "", age: age, gender: gender)
-    }
-    var description: String{
-        return "name:\(self.fullName) age:\(self.age) gender:\(self.gender)"
-    }
-    //重载==
-    static func ==(x:Person,y:Person) ->Bool{
-        return x.fullName==y.fullName&&x.age==y.age&&x.gender==y.gender
-    }
-    //重载！=
-    static func != (x:Person,y:Person) ->Bool
-    {
-        return !(x == y)
-    }
-    func run(){
-        print("Person \(self.fullName) is running")
+extension Int{
+    func sqrt1 () -> Double{
+        return sqrt(Double(self))
     }
 }
-
-var person2 = Person(firstName:"wang",age:40,gender:Gender.male)
-var person1 = Person(firstName: "Li", lastName: " guiyang", age: 18, gender: Gender.female)
-print("person1==person2:")
-print(person1==person2)
-print("person1!=person2:")
-print(person1 != person2)
-//继承Person，SchoolProtocol
-class Teacher: Person,SchoolProtocol{
-    var title : String
-    var department: Department //= Department.Computer
-    func lendBook(){
-        print("\(self.fullName) teacher lended a related \(self.department) book")
+let extenresult = 16.sqrt1()
+print("输出结果（扩展了Int）：\(extenresult)")
+//let result = integer.reduce((max:Int.min,min:Int.max,sum:0)){
+//    (result,i) in return (max(result.max,i),min(result.min,i),result.sum+i)
+//}
+func comMaxMin <T: Comparable>(number:T...) ->(max:T,min:T){
+    var max = number[0]
+    var min = number[0]
+    for i in number{
+        if i < min {
+            min = i
+        }
+        if i>max {
+            max = i
+        }
     }
-    //初始化
-    init(title: String, firstName: String, lastName: String, age: Int, gender: Gender,department:Department){
-        self.title = title
-        self.department = department
-        super.init(firstName: firstName, lastName: lastName, age: age, gender: gender)
-    }
-    override var description: String{
-        return "title:\(title) name:\(fullName) age:\(age) gender:\(gender) department:\(department)"
-    }
-    override func run(){
-        print("Teacher \(self.fullName) is running")
-    }
+    return (max,min)
 }
-//Student继承Person，协议SchoolProtocol
-class Student: Person,SchoolProtocol{
-    var stuNo: String
-    var department: Department //= Department.Math
-    func lendBook(){
-        print("student \(fullName) lended a related \(department) book")
-    }
-    //初始化
-    init(stuNo:String, firstName: String,lastName: String, age: Int, gender: Gender,department:Department){
-        self.stuNo = stuNo
-        self.department = department
-        super.init(firstName: firstName, lastName: lastName, age: age, gender: gender)
-    }
-    override var description: String{
-        return "stuNo:\(stuNo) name:\(fullName) age:\(age) gender:\(gender) department:\(department) "
-    }
-    override func run(){
-        print("Student \(self.fullName) is running")
-    }
-}
-var student1 = Student(stuNo:"2016110001", firstName: "wang",lastName: " yu", age: 18, gender: Gender.male,department:Department.Math)
-print(student1)
-var teacher = Teacher(title: "Listenning", firstName: "wang", lastName: " feng", age: 23, gender: Gender.female,department:Department.Computer)
-print(teacher)
-//对象数组
-var arry = [person1,person2,teacher,student1]
-for i in 0...2{
-    let temp = Person(firstName:"li",lastName:" si",age: 18+i,gender:Gender.female)
-    arry.append(temp)
-}
-//声明一个字典并对其初始化
-var countDate = ["Person": 0,"Teacher":0,"Student":0]
-//判断各种类的个数
-for item in arry {
-    if item is Teacher {  //是否是Teacher类
-        countDate["Teacher"]! += 1
-    } else if item is Student {  //是否是Student
-        countDate["Student"]! += 1
-    } else {  //Person类
-        countDate["Person"]! += 1
-    }
-}
-//输出字典内容
-print(countDate)
-//对数组按以下要求排序并输出：age、fullName、gender+age
-//按照age
-arry.sort{return $0.age>$1.age}
-print("按照age排序")
-for i in 0...arry.count-1
-{
-    print(arry[i])
-}
-//按照fullName
-arry.sort{return $0.fullName>$1.fullName}
-print("按照fullName排序")
-for i in 0...arry.count-1
-{
-    print(arry[i])
-}
-//按照gender+age
-func compareGender(x:Gender,y:Gender) ->Bool
-{
-    return x.hashValue > y.hashValue
-}
-func compare(x:Person,y:Person) ->Bool {
-    return compareGender(x:x.gender,y:y.gender) && x.age>y.age
-}
-arry.sort(by:compare)
-print("按照gender+age排序")
-for i in 0...arry.count-1
-{
-    print(arry[i])
-}
-//对数组进行穷举。
-//调用每个对象的run方法,满足协议SchoolProtocol对象的lendBook
-for item in arry
-{
-    item.run()
-    if let teacher = item as? Teacher{
-        teacher.lendBook()
-    }
-    else if let student = item as? Student{
-        student.lendBook()
-    }
-}
-
-
-
-
+let tempInt = comMaxMin(number:2,3,4,5,6)
+print("最大值：\(tempInt.max)")
+print("最小值：\(tempInt.min)")
 
